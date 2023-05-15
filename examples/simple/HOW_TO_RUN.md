f0420dd623b595cdc8f1225072316b7ae914313e
Ippei Obayashi <ohai@kmc.gr.jp>
2023-05-15 11:04:59 +0900

    Add simple demo

---
+++ examples/simple/HOW_TO_RUN.md
@@ -0,0 +1,7 @@
+Type the following
+
+    bundle install
+    bundle exec rbs collection install
+    bundle exec rake sig
+    
+and "sig/demo.rbs" is generated.
---
+++ examples/simple/lib/demo.rb
@@ -0,0 +1,11 @@
+class Demo
+  # Returns x + y
+  # 
+  # @param x [Integer] left parameter
+  # @param y [Integer] right parameter
+  #
+  # @return [Integer] sum of x and y
+  def plus(x, y)
+    x + y
+  end
+end
